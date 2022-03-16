9ac3e6da8947ff83cdb0dcea18a7a4b116708cc4
GxdTxnz <61706715+GxdTxnz@users.noreply.github.com>
2022-03-16 21:38:04 +0300

    Create RBTNode.java

---
+++ labs2/2/RBTNode.java
@@ -0,0 +1,26 @@
+public class RBTNode<T extends Comparable<T>> 
+{
+        private static final boolean RED   = false;
+        private static final boolean BLACK = true;
+        boolean color;        
+        T key;               
+        RBTNode<T> left;    
+        RBTNode<T> right;   
+        RBTNode<T> parent;   
+
+        public RBTNode(T key, boolean color, RBTNode<T> parent, RBTNode<T> left, RBTNode<T> right) {
+            this.key = key;
+            this.color = color;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public String toString() {
+            return ""+key+(this.color==RED?"(R)":"B");
+        }
+    }
