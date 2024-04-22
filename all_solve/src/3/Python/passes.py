b47e3e671a6c523aa3386147d5b768be98cdb220
GxdTxnz <61706715+GxdTxnz@users.noreply.github.com>
2024-04-22 23:01:18 +0300

    Update passes.py

--- all_solve/src/3/Python/passes.py
+++ all_solve/src/3/Python/passes.py
@@ -1,14 +1,29 @@
 import itertools
 import os
+from math import factorial
+from tqdm import tqdm
 
 s = input("Введите пароль: ")
-chars = set(s)
 n = input("Введите имя: ")
-b = int(input("Минимальная длина: "))
-os.mkdir(f'Passes_{n}')
+min_length = int(input("Минимальная длина: "))
+max_length = int(input("Максимальная длина: "))
 
-for i in range(b, len(s) + 1):
-	with open(f'Passes_{n}/comb_{i}.txt', 'w') as file:
-		for combo in itertools.permutations(chars, i):
-			file.write("".join(combo) + "\n")
-print("Все пароли сгенерированы")
+# Создаем директорию для хранения файлов с комбинациями
+directory = f'Passes_{n}'
+os.makedirs(directory, exist_ok=True)
+
+# Вычисляем общее количество комбинаций для прогресс бара
+total_combinations = 0
+for length in range(min_length, max_length + 1):
+    total_combinations += factorial(len(s)) // factorial(len(s) - length)
+
+# Генерируем и записываем комбинации различной длины без повторяющихся символов
+for length in range(min_length, max_length + 1):
+    with open(f'{directory}/comb_{length}.txt', 'w') as file:
+        combinations = itertools.permutations(s, length)
+        with tqdm(total=total_combinations, desc=f'Генерация комбинаций длины {length}', unit='комбинации') as pbar:
+            for combo in combinations:
+                file.write("".join(combo) + "\n")
+                pbar.update(1)
+
+print("Все пароли сгенерированы и сохранены в соответствующие файлы.")
